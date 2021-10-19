16019dad1880205cf2e63b3a243fa41ca1ebba7c
Jean Kei <jeankei@yandex.ru>
2021-10-19 23:14:40 +0900

    меню готово

--- js/main.js
+++ js/main.js
@@ -46,3 +46,14 @@ const swiper = new Swiper('.swiper', {
     }
   }
 });
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.navbar');
+const CloseMenuButton = document.querySelector('.navbar');
+
+menuButton.addEventListener("click", (event) => {
+  menu.classList.toggle("is-open");
+});
+CloseMenuButton.addEventListener("click", (event) => {
+  menu.classList.toggle("is-open");
+});
